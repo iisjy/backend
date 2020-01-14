8e9dd7fb0cc82349cfd24f45d3558837b2652bec
realJingYang <jingyang@iisjy.cn>
2020-01-14 10:50:06 +0800

    add code

--- src/cn/iisjy/main/entity/iImage.java
+++ src/cn/iisjy/main/entity/iImage.java
@@ -1,5 +1,10 @@
 package cn.iisjy.main.entity;
 
 public class iImage {
-	
+	public String	img_hash;
+	public String	img_location;
+	public String	img_type;
+	public String	img_title;
+	public String	img_alt;
+	public int		img_createor;
 }
