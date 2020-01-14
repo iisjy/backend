8f638d50a897be6ff2b9396344618b89d6082d52
realJingYang <jingyang@iisjy.cn>
2020-01-14 11:24:49 +0800

    add code

--- src/cn/iisjy/main/dao/InitSQL.java
+++ src/cn/iisjy/main/dao/InitSQL.java
@@ -2,7 +2,6 @@ package cn.iisjy.main.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 
 public class InitSQL {
 	
--- src/cn/iisjy/main/utility/BehaviorRecord.java
+++ src/cn/iisjy/main/utility/BehaviorRecord.java
@@ -1,5 +1,16 @@
 package cn.iisjy.main.utility;
 
 public class BehaviorRecord {
+	public static void regRecord() {
 
+	}
+	public static void loginRecord() {
+		
+	}
+	public static void publishRecord() {
+		
+	}
+	public static void replyRecord() {
+		
+	}
 }
