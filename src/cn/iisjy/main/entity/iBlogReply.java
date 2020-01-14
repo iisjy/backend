a02d606b94cefda5b908559386ea58110754ad1d
realJingYang <jingyang@iisjy.cn>
2020-01-14 10:53:45 +0800

    add code

--- src/cn/iisjy/main/entity/iBlogReply.java
+++ src/cn/iisjy/main/entity/iBlogReply.java
@@ -1,5 +1,12 @@
 package cn.iisjy.main.entity;
 
-public class iBlogReply {
+import java.sql.Timestamp;
 
+public class iBlogReply {
+	public int			reply_article;
+	public int			reply_floor;
+	public int			reply_author;
+	public Timestamp	reply_time;
+	public String		reply_content;
+	public String		reply_status;
 }
