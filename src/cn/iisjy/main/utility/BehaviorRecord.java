6fa67078c4caec73d4b7043a5fd42065521fbead
realJingYang <jingyang@iisjy.cn>
2020-01-14 11:51:19 +0800

    add code

--- src/cn/iisjy/main/utility/BehaviorRecord.java
+++ src/cn/iisjy/main/utility/BehaviorRecord.java
@@ -1,16 +1,29 @@
 package cn.iisjy.main.utility;
 
 public class BehaviorRecord {
-	public static void regRecord() {
-
+	public static void regRecord() {		//永久保留
+		//用户唯一标识
+		//用户名称
+		//注册时间
+		//IP地址、端口
+		//设备（web:浏览器UA；app:设备识别码）
 	}
-	public static void loginRecord() {
-		
+	public static void loginRecord() {		//保存12个月
+		//用户唯一标识
+		//登陆时间
+		//登陆方式（用户名、手机号、QQ授权、微信授权）
+		//IP地址、端口
+		//设备（web:浏览器UA；app:设备识别码）
 	}
-	public static void publishRecord() {
-		
+	public static void publishRecord() {	//保存6个月
+		//用户唯一标识
+		//信息表示
+		//发布时间
+		//IP地址、端口
+		//信息摘要
+		//设备（web:浏览器UA；app:设备识别码）
 	}
-	public static void replyRecord() {
+	public static void replyRecord() {		//保存6个月
 		
 	}
 }
