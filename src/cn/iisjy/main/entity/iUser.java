28a25fc4f0ecd46bfb3aae0836556f4eb53a90b0
realJingYang <jingyang@iisjy.cn>
2020-01-14 10:59:25 +0800

    add code

--- src/cn/iisjy/main/entity/iUser.java
+++ src/cn/iisjy/main/entity/iUser.java
@@ -3,19 +3,20 @@ package cn.iisjy.main.entity;
 import java.sql.Timestamp;
 
 public class iUser {
-	public int		user_id;
-	public String	user_name;
-	public String	user_pswd_hash;
-	public String	user_pswd_salt;
-	public String	user_login_phone;
-	public String	user_login_email;
-	public String	user_real_name;
-	public int		user_certificate;
-	public String	user_avatar;
-	public char 	user_sex;
-	public Timestamp	user_birthdate;
-	public Timestamp	user_regtime;
-	public String	user_login_token_qq;
-	public String	user_login_token_wechat;
+	public int			user_id;					//用户身份识别码
+	public String		user_name;					//用户昵称
+	public String		user_pswd_hash;				//密码哈希值（SHA-512）
+	public String		user_pswd_salt;				//用户的盐值
+	public String		user_login_phone;			//登陆用的手机号
+	public String		user_login_email;			//登陆用的电子邮箱
+	public String		user_real_name;				//用户真实姓名
+	public int			user_certificate;			//用户证件信息
+	public String		user_avatar;				//用户头像（哈希）
+	public char 		user_sex;					//用户性别
+	public Timestamp	user_birthdate;				//用户出生日期
+	public Timestamp	user_regtime;				//注册时间
+	public String		user_regIPaddr;				//注册IP地址
+	public String		user_login_token_qq;
+	public String		user_login_token_wechat;
 	
 }
